106704173570a5badeed1d2c3e4104203668ec1a
Tim M <timothee01meyer@gmail.com>
2025-01-28 08:46:47 +0100

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by meyer timothee on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
